4c5c54f37e6148d7d3d6bddd759015bd7ff56896
RanXuCan <ranxucan@163.com>
2020-09-19 19:14:11 +0800

    modified FileTest.java,fix some known errors

--- src/main/java/com/rxc/sourcecodelearning/FileTest.java
+++ src/main/java/com/rxc/sourcecodelearning/FileTest.java
@@ -1,10 +1,12 @@
 package com.rxc.sourcecodelearning;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * @Description: display all files in a special directory
+ * @Description: display all files in a special directory,then output to a special file(containsFile.txt) in this directory
  * @Author RanXuCan
  * @Date 2020/9/19 18:00
  */
@@ -14,20 +16,37 @@ public class FileTest {
         System.out.println("please input the path of the special directory");
         String filename = scanner.next();
         File file = new File(filename);
-        if (!file.exists()) {
-            System.out.println("directory not found");
-        } else
-            displayFiles(file);
+        File outputTo = new File(file, "containsFile.txt");
+        FileWriter fos = null;
+        try {
+            fos = new FileWriter(outputTo);
+            if (!file.exists()) {
+                System.out.println("directory not found");
+            } else {
+                displayFiles(file, fos);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fos.flush();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void displayFiles(File filename) {
+    public static void displayFiles(File filename, FileWriter fos) throws IOException {
         File[] listFiles = filename.listFiles();
         if (listFiles != null && listFiles.length > 0) {
             for (File file : listFiles) {
                 if (file.isFile()) {
-                    System.out.println(file.getAbsolutePath());
+                    String str = file.getAbsolutePath();
+                    System.out.println(str);
+                    fos.write(str + "\n");
                 } else {
-                    displayFiles(file);
+                    displayFiles(file, fos);
                 }
             }
         } else {
